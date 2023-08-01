f58b376fc15eb3d6883fc6d6a1c7342415c499ec
Nic Tau <nicholas.tauroa@developersinstitute.co.nz>
2023-08-01 17:07:38 +1200

    Part C, level 3

--- Submission/script.js
+++ Submission/script.js
@@ -12,9 +12,7 @@ const getBotReply = (msg) => {
   if (level === 0) {
     userName = msg;
     level += 1;
-    return `Hello ${userName} lets see if I can help you with your plant today
-    
-    Is your plant wilting?`
+    return `Hello ${userName} lets see if I can help you with your plant today. Is your plant wilting?`
   }
 
   if (msg === "yes" || msg === "no") {
@@ -23,17 +21,34 @@ const getBotReply = (msg) => {
     if (level === 1) {
       level += 1;
       if (lastResponse === "yes") {
-        return "Is the oil dry?"
+        return "Is the Soil dry?"
       } else {
         return "Are the leaves turning yellow?"
       }
     }
+
+    if (level === 2) {
+      level += 1
+      if (lastResponse === "yes") {
+        return "Water it. If it is still unhappy tomorrow ask again"
+      } else {
+        return "Does the soil smell bad?"
+      }
+    }
+
+    if (level === 3) {
+      level += 1
+      if (lastResponse === "yes") {
+        return `${userName} it sounds like you have mould. I suggest you pot with new soil.`
+      } else {
+        return `It might sound funny ${userName}, but your plant is likely too hot or cold. Try moving it to a different location and see how it goes.`
+      }
+    }
+    
   }
 
   if (msg === "help") {
-    return `Hi  ${userName}, I see you need some help
-    Just type 'restart' to start again.
-    ` 
+    return `Hi ${userName}, I see you need some help. Just type 'restart' to start again.` 
   }
 
   if (msg === "restart") {
--- test/script.test.js
+++ test/script.test.js
@@ -11,11 +11,70 @@ beforeEach(() => {
 });
 
 describe("getBotReply", () => {
-  it("should reply with [PUT YOUR DESCRIPTION HERE]", () => {
+  it("should reply with 'Harry, yes yes'", () => {
     const botReply1 = getBotReply("Harry");
-    const expectedReply1 = "";
+    const expectedReply1 =
+      "Hello Harry lets see if I can help you with your plant today. Is your plant wilting?";
+    expect(botReply1).toEqual(expectedReply1);
 
+    const botReply2 = getBotReply("yes");
+    const expectedReply2 = "Is the Soil dry?";
+    expect(botReply2).toEqual(expectedReply2);
+
+    const botReply3 = getBotReply("yes");
+    const expectedReply3 =
+      "Water it. If it is still unhappy tomorrow ask again";
+    expect(botReply3).toEqual(expectedReply3);
     // Uncomment the following line and update your expectation
     // expect(botReply1).toEqual(expectedReply1);
   });
 });
+
+describe("getBotReply", () => {
+  it("should reply with 'Harry, yes no yes'", () => {
+    const botReply1 = getBotReply("Harry");
+    const expectedReply1 =
+      "Hello Harry lets see if I can help you with your plant today. Is your plant wilting?";
+    expect(botReply1).toEqual(expectedReply1);
+
+    const botReply2 = getBotReply("yes");
+    const expectedReply2 = "Is the Soil dry?";
+    expect(botReply2).toEqual(expectedReply2);
+
+    const botReply3 = getBotReply("no");
+    const expectedReply3 = "Does the soil smell bad?";
+    expect(botReply3).toEqual(expectedReply3);
+
+    const botReply4 = getBotReply("yes");
+    const expectedReply4 =
+      "Harry it sounds like you have mould. I suggest you pot with new soil.";
+    expect(botReply4).toEqual(expectedReply4);
+    // Uncomment the following line and update your expectation
+    // expect(botReply1).toEqual(expectedReply1);
+  });
+});
+
+describe("getBotReply", () => {
+  it("should reply with 'Harry, yes no no'", () => {
+    const botReply1 = getBotReply("Harry");
+    const expectedReply1 =
+      "Hello Harry lets see if I can help you with your plant today. Is your plant wilting?";
+    expect(botReply1).toEqual(expectedReply1);
+
+    const botReply2 = getBotReply("yes");
+    const expectedReply2 = "Is the Soil dry?";
+    expect(botReply2).toEqual(expectedReply2);
+
+    const botReply3 = getBotReply("no");
+    const expectedReply3 = "Does the soil smell bad?";
+    expect(botReply3).toEqual(expectedReply3);
+
+    const botReply4 = getBotReply("no");
+    const expectedReply4 =
+      "It might sound funny Harry, but your plant is likely too hot or cold. Try moving it to a different location and see how it goes.";
+    expect(botReply4).toEqual(expectedReply4);
+    // Uncomment the following line and update your expectation
+    // expect(botReply1).toEqual(expectedReply1);
+  });
+});
+
